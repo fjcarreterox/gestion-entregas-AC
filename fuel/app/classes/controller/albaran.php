b4feb02c7f3f15019db8eb60ed01235f2775ccc8
Javi Carretero <fjcarretero@ximdex.com>
2015-07-03 20:30:32 +0200

    Allowing comments on delivery notes

--- fuel/app/classes/controller/albaran.php
+++ fuel/app/classes/controller/albaran.php
@@ -45,18 +45,19 @@ class Controller_Albaran extends Controller_Template
 					'idalbaran' => Input::post('idalbaran'),
 					'identrega' => Input::post('identrega'),
 					'idproveedor' => Input::post('idproveedor'),
+                    'comentario' => Input::post('comentario'),
 				));
 
 				if ($albaran and $albaran->save())
 				{
-					Session::set_flash('success', 'Added albaran #'.$albaran->id.'.');
+					Session::set_flash('success', 'Albaran Núm.'.$albaran->id.' creado correctamente.');
 
-					Response::redirect('albaran');
+					Response::redirect('albaran/list');
 				}
 
 				else
 				{
-					Session::set_flash('error', 'Could not save albaran.');
+					Session::set_flash('error', 'No se pudo almacenar el albarán.');
 				}
 			}
 			else
@@ -65,19 +66,18 @@ class Controller_Albaran extends Controller_Template
 			}
 		}
 
-		$this->template->title = "Albarans";
+		$this->template->title = "Albaranes";
 		$this->template->content = View::forge('albaran/create');
 
 	}
 
 	public function action_edit($id = null)
 	{
-		is_null($id) and Response::redirect('albaran');
+		is_null($id) and Response::redirect('albaran/list');
 
-		if ( ! $albaran = Model_Albaran::find($id))
-		{
-			Session::set_flash('error', 'Could not find albaran #'.$id);
-			Response::redirect('albaran');
+		if ( ! $albaran = Model_Albaran::find($id)){
+			Session::set_flash('error', 'No existe en el sistema el albarán núm. '.$id);
+			Response::redirect('albaran/list');
 		}
 
 		$val = Model_Albaran::validate('edit');
@@ -87,16 +87,13 @@ class Controller_Albaran extends Controller_Template
 			$albaran->idalbaran = Input::post('idalbaran');
 			$albaran->identrega = Input::post('identrega');
 			$albaran->idproveedor = Input::post('idproveedor');
+            $albaran->comentario = Input::post('comentario');
 
-			if ($albaran->save())
-			{
-				Session::set_flash('success', 'Updated albaran #' . $id);
-
-				Response::redirect('albaran');
+			if ($albaran->save()){
+				Session::set_flash('success', 'Albarán núm. ' . $id .' actualizado.');
+				Response::redirect('albaran/list');
 			}
-
-			else
-			{
+			else{
 				Session::set_flash('error', 'Could not update albaran #' . $id);
 			}
 		}
@@ -108,36 +105,30 @@ class Controller_Albaran extends Controller_Template
 				$albaran->idalbaran = $val->validated('idalbaran');
 				$albaran->identrega = $val->validated('identrega');
 				$albaran->idproveedor = $val->validated('idproveedor');
-
+                $albaran->comentario = $val->validated('comentario');
 				Session::set_flash('error', $val->error());
 			}
 
 			$this->template->set_global('albaran', $albaran, false);
 		}
 
-		$this->template->title = "Albarans";
+		$this->template->title = "Albaranes";
 		$this->template->content = View::forge('albaran/edit');
 
 	}
 
 	public function action_delete($id = null)
 	{
-		is_null($id) and Response::redirect('albaran');
+		is_null($id) and Response::redirect('albaran/list');
 
-		if ($albaran = Model_Albaran::find($id))
-		{
+		if ($albaran = Model_Albaran::find($id)){
 			$albaran->delete();
-
-			Session::set_flash('success', 'Deleted albaran #'.$id);
+			Session::set_flash('success', 'Albarán núm. '.$id. ' borrado.');
 		}
-
-		else
-		{
-			Session::set_flash('error', 'Could not delete albaran #'.$id);
+		else{
+			Session::set_flash('error', 'No se pudo borrar el albarán núm. '.$id);
 		}
-
-		Response::redirect('albaran');
-
+		Response::redirect('albaran/list');
 	}
 
 }
