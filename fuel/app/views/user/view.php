cd9e3cb54ebb6002790108953b08f2c96b18e98b
Javi Carretero <fjcarretero@ximdex.com>
2015-07-10 19:06:30 +0200

    Hidding user pass on view user template

--- fuel/app/views/user/view.php
+++ fuel/app/views/user/view.php
@@ -1,11 +1,10 @@
 <h2>Mostrando los datos del <span class='muted'>usuario <?php /*echo $user->id;*/ ?></span>seleccionado:</h2>
-
+<br/>
 <p>
 	<strong>Nombre de usuario:</strong>
 	<?php echo $user->username; ?></p>
 <p>
-	<strong>Contraseña:</strong>
-	<?php echo $user->pass; ?></p>
-
+	<strong>Puesto de recogida:</strong>
+	<?php echo Model_Puesto::find($user->idpuesto)->get('nombre'); ?></p>
 <?php echo Html::anchor('user/edit/'.$user->id, 'Editar'); ?> |
 <?php echo Html::anchor('user', 'Volver'); ?>
