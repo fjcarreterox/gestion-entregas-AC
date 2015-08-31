c822d040de433b031160e5d5493ab47c9667c0f5
Javi Carretero <fjcarretero@ximdex.com>
2015-08-31 17:06:39 +0200

    Print button on deliveries search page

--- fuel/app/views/entrega/fechas.php
+++ fuel/app/views/entrega/fechas.php
@@ -2,8 +2,9 @@
     echo Form::open(array("class" => "form-horizontal"));
     echo "<h2 > Selecciona un rango de fechas para obtener el listado de entregas de <b>".Model_Puesto::find($idpuesto)->get('nombre')."</b></h2 >";
 ?>
-<p>Para obtener resultados de entregas, asegurate de que la fecha final es posterior a la inicial.</p>
-    <fieldset>
+<br/>
+<p class="hide_elem">Para obtener resultados de entregas, asegurate de que la fecha final es posterior a la inicial.</p>
+    <fieldset class="hide_elem">
         <div class="form-group">
             <?php echo Form::label('Fecha inicial', 'start', array('class'=>'control-label')); ?>
             <?php echo Form::input('start', '', array('type'=>'date','class' => 'col-md-4 form-control')); ?>
@@ -20,7 +21,8 @@
     </fieldset>
     <?php echo Form::close();
     if(count($entregas)>0):
-        echo "<p>Mostrando listado de entregas realizadas entre el <b>".date_conv($_POST["start"])."</b> y el <b>".date_conv($_POST["end"])."</b>.</p>";
+        echo Html::anchor('javascript:window.print()', '<i class="icon-trash icon-white"></i> Imprimir entradas', array('class' => 'btn btn-small btn-info','id'=>'print-invoice'));
+        echo "<br/><br/><p>Mostrando listado de entregas realizadas entre el <b>".date_conv($_POST["start"])."</b> y el <b>".date_conv($_POST["end"])."</b>.</p>";
         echo "<p>Total de entregas encontradas: <b>".count($entregas)."</b></p>";
     ?>
 <table class="table table-striped">
@@ -48,8 +50,6 @@
             <div class="btn-toolbar">
                 <div class="btn-group">
                     <?php echo Html::anchor('entrega/view/'.$item->id, '<i class="icon-eye-open"></i> Ver detalle', array('class' => 'btn btn-small')); ?>
-                    <?php /*echo Html::anchor('entrega/edit/'.$item->id, '<i class="icon-wrench"></i> Editar', array('class' => 'btn btn-small'));*/ ?>
-                    <?php /*echo Html::anchor('entrega/delete/'.$item->id, '<i class="icon-trash icon-white"></i> Borrar', array('class' => 'btn btn-small btn-danger', 'onclick' => "return confirm('¿Estás seguro de esto?')"));*/ ?>
                 </div>
             </div>
 
