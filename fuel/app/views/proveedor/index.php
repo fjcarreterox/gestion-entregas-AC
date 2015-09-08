f3588c33d9be8144dfc80192a1e888ad7606e8d0
Javi Carretero <fjcarretero@ximdex.com>
2015-09-08 23:57:24 +0200

    Improve provider list with gliphicons

--- fuel/app/views/proveedor/index.php
+++ fuel/app/views/proveedor/index.php
@@ -1,9 +1,11 @@
 <h2><span class='muted'>Proveedores</span> de la campaña 2015</h2>
-<br>
+<br/>
+<p>Durante la presente campaña hay registrados <strong><?php echo count($proveedors);?> proveedores.</strong></p>
+<br/>
 <p>
-    <?php echo Html::anchor('proveedor/create', 'Añadir nuevo', array('class' => 'btn btn-success')); ?>
-    <?php echo Html::anchor('proveedor/search', 'Buscar proveedor', array('class' => 'btn btn-success')); ?>
-    <?php echo Html::anchor('entrega', 'Nueva entrega', array('class' => 'btn btn-success')); ?>
+    <?php echo Html::anchor('proveedor/create', '<span class="glyphicon glyphicon-plus"></span> Añadir nuevo', array('class' => 'btn btn-success')); ?>
+    <?php echo Html::anchor('proveedor/search', '<span class="glyphicon glyphicon-search"></span> Buscar proveedor', array('class' => 'btn btn-info')); ?>
+    <?php echo Html::anchor('entrega', '<span class="glyphicon glyphicon-plus"></span> Nueva entrega', array('class' => 'btn btn-success')); ?>
 </p>
 <?php if ($proveedors): ?>
 <table class="table table-striped">
@@ -34,11 +36,11 @@
 			<td>
 				<div class="btn-toolbar">
 					<div class="btn-group">
-						<?php echo Html::anchor('proveedor/view/'.$item->id, '<i class="icon-eye-open"></i> Ver ficha completa', array('class' => 'btn btn-small')); ?>
-                        <?php echo Html::anchor('proveedor/edit/'.$item->id, '<i class="icon-wrench"></i> Editar', array('class' => 'btn btn-small')); ?>
-                        <?php echo Html::anchor('entrega/list_prov/'.$item->id, '<i class="icon-trash icon-white"></i> Ficha final', array('class' => 'btn btn-small btn-success')); ?>
-                        <?php echo Html::anchor('anticipo/list_prov/'.$item->id, '<i class="icon-trash icon-white"></i> Anticipos', array('class' => 'btn btn-small btn-info')); ?>
-                        <?php echo Html::anchor('proveedor/delete/'.$item->id, '<i class="icon-trash icon-white"></i> Borrar', array('class' => 'btn btn-small btn-danger', 'onclick' => "return confirm('¿Estás seguro de esto?')")); ?>
+						<?php echo Html::anchor('proveedor/view/'.$item->id, '<span class="glyphicon glyphicon-eye-open"></span> Ver ficha', array('class' => 'btn btn-sm btn-default')); ?>
+                        <?php echo Html::anchor('proveedor/edit/'.$item->id, '<span class="glyphicon glyphicon-pencil"></span> Editar', array('class' => 'btn btn-small btn-success')); ?>
+                        <?php echo Html::anchor('entrega/list_prov/'.$item->id, '<span class="glyphicon glyphicon-paperclip"></span> Ficha final', array('class' => 'btn btn-small btn-warning')); ?>
+                        <?php echo Html::anchor('anticipo/list_prov/'.$item->id, '<span class="glyphicon glyphicon-euro"></span> Anticipos', array('class' => 'btn btn-small btn-info')); ?>
+                        <?php echo Html::anchor('proveedor/delete/'.$item->id, '<span class="glyphicon glyphicon-trash"></span> Borrar', array('class' => 'btn btn-small btn-danger', 'onclick' => "return confirm('¿Estás seguro de esto? El borrado de un proveedor conllevará al borrado de sus facturas, anticipos, albaranes y entregas asociadas.')")); ?>
                     </div>
 				</div>
 			</td>
@@ -50,7 +52,7 @@
 <p>No se han encontrado proveedores.</p>
 
 <?php endif; ?><p>
-	<?php echo Html::anchor('proveedor/create', 'Añadir nuevo', array('class' => 'btn btn-success')); ?>
-    <?php echo Html::anchor('proveedor/search', 'Buscar proveedor', array('class' => 'btn btn-success')); ?>
-    <?php echo Html::anchor('entrega', 'Nueva entrega', array('class' => 'btn btn-success')); ?>
+    <?php echo Html::anchor('proveedor/create', '<span class="glyphicon glyphicon-plus"></span> Añadir nuevo', array('class' => 'btn btn-success')); ?>
+    <?php echo Html::anchor('proveedor/search', '<span class="glyphicon glyphicon-search"></span> Buscar proveedor', array('class' => 'btn btn-info')); ?>
+    <?php echo Html::anchor('entrega', '<span class="glyphicon glyphicon-plus"></span> Nueva entrega', array('class' => 'btn btn-success')); ?>
 </p>
