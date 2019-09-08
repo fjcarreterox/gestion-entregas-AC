9d96501bdd07e00ddbc5de00dd351028695171ea
Javi Carretero <rentonr11@gmail.com>
2019-09-08 09:39:15 +0200

    Changing the formula for sizes

--- fuel/app/views/entrega/list_prov.php
+++ fuel/app/views/entrega/list_prov.php
@@ -36,9 +36,9 @@ foreach ($entregas as $item):?>
             <td><?php echo Model_Albaran::find($item->albaran)->get('idalbaran'); ?></td>
 			<td><?php echo Model_Variedad::find($item->variedad)->get('nombre');?></td>
             <td class="gris"><?php echo $item->tam;
-                if($item->tam!=0) {
-                    $total_tam[$item->variedad][] = $item->tam;
-                }
+
+                $total_tam[$item->variedad][] = $item->tam*$item->total;
+
                 ?></td>
 			<td><strong><?php echo $item->total;
                 if(isset($total_variedades[$item->variedad])) {
@@ -64,7 +64,8 @@ foreach ($entregas as $item):?>
 <br/>
     <br/>
 <h3 class="print"><u><b>Tamaños medio</b> por variedad de aceituna</u></h3>
-<table class="table table-striped print">
+<?php if (!empty($total_tam)): ?>
+    <table class="table table-striped print">
     <thead>
     </thead>
     <tbody>
@@ -72,11 +73,14 @@ foreach ($entregas as $item):?>
     foreach ($total_tam as $v => $value):?>
         <tr>
             <td><?php echo "Tamaño medio ".Model_Variedad::find($v)->get('nombre'); ?></td>
-            <td><?php echo array_sum($value)/count($value);?></td>
+            <td><?php echo number_format(array_sum($value)/$total_variedades[$v],4);?></td>
         </tr>
     <?php endforeach;?>
     </tbody>
     </table>
+    <?php else: ?>
+        <p class="print">No se han registrado aún entregas con tamaños significativos para este proveedor.</p>
+    <?php endif; ?>
     <br/>
     <br/>
     <h3 class="print"><u>Resumen de <b>kg. entregados</b> por variedad de aceituna</u></h3>
