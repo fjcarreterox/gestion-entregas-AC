0e1bc29341d4a42a54979cc1307f624c40082aec
Javi Carretero <fjcarretero.garcia@gmail.com>
2024-11-29 17:05:15 +0100

    New ranges for 2024

--- fuel/app/views/entrega/list.php
+++ fuel/app/views/entrega/list.php
@@ -42,10 +42,10 @@ else{
         $tam_total = array();
         $num_entregas = array();
         $total_kg_tam = array();
-        //$rep_m = array("O"=>0,"A"=>0,"B"=>0,"C"=>0,"D"=>0,"E"=>0,"F"=>0,"G"=>0,"H"=>0,"I"=>0,"J"=>0);
-        $rep_m = array("O"=>0,"A"=>0,"B"=>0,"C"=>0,"D"=>0);
-        $rep_g = array("O"=>0,"A"=>0,"B"=>0,"C"=>0);
-        $rango_molino = array("R1"=>0,"R2"=>0,"R3"=>0/*,"R4"=>0*/);
+        $rep_m = array("O"=>0,"A"=>0,"B"=>0,"C"=>0,"D"=>0,"E"=>0,"F"=>0/*,"G"=>0,"H"=>0,"I"=>0,"J"=>0*/);
+        //$rep_m = array("O"=>0,"A"=>0,"B"=>0,"C"=>0,"D"=>0);
+        $rep_g = array("O"=>0,"A"=>0,"B"=>0,"C"=>0,"D"=>0,"E"=>0);
+        $rango_molino = array("R1"=>0,"R2"=>0,"R3"=>0,"R4"=>0,"R5"=>0,"R6"=>0,"R7"=>0);
         foreach ($entregas as $item):
             if(Model_Albaran::find('first', array('where' => array('id' => $item->albaran)))){
                 $alb = Model_Albaran::find('first', array('where' => array('id' => $item->albaran)));
@@ -59,32 +59,35 @@ else{
 
             if($item->variedad==1){
                 if($item->tam == 0) $rep_m["O"] += $item->total;
-                if(($item->tam > 0) && ($item->tam <= 240)) $rep_m["A"] += $item->total;
-                if(($item->tam > 240) && ($item->tam <= 260)) $rep_m["B"] += $item->total;
-                if(($item->tam > 260) && ($item->tam <= 300)) $rep_m["C"] += $item->total;
-                //if(($item->tam > 250) && ($item->tam <= 260)) $rep_m["C"] += $item->total;
-                /*if(($item->tam > 260) && ($item->tam <= 270)) $rep_m["D"] += $item->total;
-                if(($item->tam > 270) && ($item->tam <= 280)) $rep_m["E"] += $item->total;
+                if(($item->tam > 0) && ($item->tam <= 220)) $rep_m["A"] += $item->total;
+                if(($item->tam > 220) && ($item->tam <= 240)) $rep_m["B"] += $item->total;
+                if(($item->tam > 240) && ($item->tam <= 260)) $rep_m["C"] += $item->total;
+                if(($item->tam > 260) && ($item->tam <= 300)) $rep_m["D"] += $item->total;
+                if(($item->tam > 300) && ($item->tam <= 320)) $rep_m["E"] += $item->total;
+                /*if(($item->tam > 270) && ($item->tam <= 280)) $rep_m["E"] += $item->total;
                 if(($item->tam > 280) && ($item->tam <= 290)) $rep_m["F"] += $item->total;
                 if(($item->tam > 290) && ($item->tam <= 300)) $rep_m["G"] += $item->total;
                 if(($item->tam > 300) && ($item->tam <= 320)) $rep_m["H"] += $item->total;
                 if(($item->tam > 320) && ($item->tam <= 340)) $rep_m["I"] += $item->total;*/
                 //else if($item->tam > 340) $rep_m["J"] += $item->total;
-                else if($item->tam > 300) $rep_m["D"] += $item->total;
+                else if($item->tam > 320) $rep_m["F"] += $item->total;
             }
             else if($item->variedad==2){
                 if($item->tam == 0) $rep_g["O"] += $item->total;
-                if(($item->tam > 0) && ($item->tam <= 120)) $rep_g["A"] += $item->total;
-                if(($item->tam > 120) && ($item->tam <= 130)) $rep_g["B"] += $item->total;
-                //if(($item->tam > 120) && ($item->tam <= 140)) $rep_g["C"] += $item->total;
-                //if(($item->tam > 140) && ($item->tam <= 160)) $rep_g["D"] += $item->total;
-                else if($item->tam > 130) $rep_g["C"] += $item->total;
+                if(($item->tam > 0) && ($item->tam <= 59)) $rep_g["A"] += $item->total;
+                if(($item->tam > 59) && ($item->tam <= 90)) $rep_g["B"] += $item->total;
+                if(($item->tam > 90) && ($item->tam <= 110)) $rep_g["C"] += $item->total;
+                if(($item->tam > 110) && ($item->tam <= 130)) $rep_g["D"] += $item->total;
+                else if($item->tam > 130) $rep_g["E"] += $item->total;
             }
             else if($item->variedad==3){
-                if(($item->fecha >= "2022-01-01") && ($item->fecha <= "2022-10-09")) $rango_molino["R1"] += $item->total;
-                if(($item->fecha >= "2022-10-10") && ($item->fecha <= "2022-10-27")) $rango_molino["R2"] += $item->total;
-                if(($item->fecha >= "2022-10-28") && ($item->fecha <= "2022-12-31")) $rango_molino["R3"] += $item->total;
-                //if(($item->fecha > "2022-11-13") && ($item->fecha <= "2022-12-31")) $rango_molino["R4"] += $item->total;
+                if(($item->fecha >= "2024-01-01") && ($item->fecha <= "2024-10-13")) $rango_molino["R1"] += $item->total;
+                if(($item->fecha >= "2024-10-14") && ($item->fecha <= "2024-10-20")) $rango_molino["R2"] += $item->total;
+                if(($item->fecha >= "2024-10-21") && ($item->fecha <= "2024-10-27")) $rango_molino["R3"] += $item->total;
+                if(($item->fecha > "2024-10-28") && ($item->fecha <= "2024-11-03")) $rango_molino["R4"] += $item->total;
+                if(($item->fecha > "2024-11-04") && ($item->fecha <= "2024-11-10")) $rango_molino["R5"] += $item->total;
+                if(($item->fecha > "2024-11-11") && ($item->fecha <= "2024-11-17")) $rango_molino["R6"] += $item->total;
+                if(($item->fecha > "2024-11-18") && ($item->fecha <= "2024-11-24")) $rango_molino["R7"] += $item->total;
             }
             ?>
             <tr>
@@ -194,10 +197,12 @@ else{
         <h4><b>Tipo Manzanilla</b></h4>
         <tr>
             <th>0</th>
-            <th>< 240</th>
+            <th>< 220</th>
+            <th>221-240</th>
             <th>241-260</th>
             <th>261-300</th>
-            <th>301-N</th>
+            <th>301-320</th>
+            <th>321-N</th>
         </tr>
         </thead>
         <tbody>
@@ -217,8 +222,10 @@ else{
         <h4><b>Tipo Gordal</b></h4>
         <tr>
             <th>0</th>
-            <th>< 120</th>
-            <th>121 - 130</th>
+            <th>< 60</th>
+            <th>60 - 90</th>
+            <th>91 - 110</th>
+            <th>111 - 130</th>
             <th>131 - N</th>
         </tr>
         </thead>
@@ -238,9 +245,13 @@ else{
         <thead>
         <h4><b>Tipo Molino</b></h4>
         <tr>
-            <th>Inicio campaña - 9/10</th>
-            <th>10/10 - 27/10</th>
-            <th>28/10 - 31/12</th>
+            <th>Inicio campaña - 13/10</th>
+            <th>14/10 - 20/10</th>
+            <th>21/10 - 27/10</th>
+            <th>28/10 - 3/11</th>
+            <th>4/11 - 10/11</th>
+            <th>11/11 - 17/11</th>
+            <th>18/11 - 24/11</th>
         </tr>
         </thead>
         <tbody>
